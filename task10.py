4ef2c533c68697135df607c5d996729bf6b55375
Aliaksei Strakhau <straxov.9942@gmail.com>
2024-11-12 20:00:11 +0300

    home work #3 (2nd solution for task10)

--- task10.py
+++ task10.py
@@ -5,3 +5,12 @@ for i in numbers:
     if numbers.count(i) == 1:
         unique_number = i
 print(f"Уникальное число в массиве {unique_number}")
+
+# нужно учитывать, сколько раз будет считаться цикл
+# (система может быть перегружена, если много раз повторяется -> оптимизируем)
+
+arr_2 = [1, 5, 2, 9, 2, 9, 1]
+unique_number_2 = 0
+for i in arr_2:
+    unique_number_2 ^= i
+print(unique_number_2)
